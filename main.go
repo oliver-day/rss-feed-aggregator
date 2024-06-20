f51be2c929f2cc9a8c8c3532f7169c61990f2363
oliver-day <oliver.day.swe@gmail.com>
2024-06-19 17:22:32 -0700

    feat: add dbUrl connection logic to main

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
@@ -8,13 +9,36 @@ import (
 	"github.com/joho/godotenv"
 
 	_ "github.com/lib/pq"
+
+	"github.com/oliver-day/rss-feed-aggregator/internal/database"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
+	}
+
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: dbQueries,
 	}
 
 	mux := http.NewServeMux()
