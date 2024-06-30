187230dd97307b9ddb6822d694f6efc680dc4b1b
oliver-day <oliver.day.swe@gmail.com>
2024-06-30 16:34:34 -0700

    feat: generate 'GetFeedFollowsForUser' sql->go code

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -5,3 +5,7 @@ INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id) VALUES (
 -- name: DeleteFeedFollow :exec
 DELETE FROM feed_follows WHERE id = $1 AND user_id = $2;
 --
+
+-- name: GetFeedFollowsForUser :many
+SELECT * FROM feed_follows WHERE user_id = $1;
+--
