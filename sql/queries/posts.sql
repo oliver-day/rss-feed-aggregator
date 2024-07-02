49a3f2c06921c02e043614b292e989f50bce493a
oliver-day <oliver.day.swe@gmail.com>
2024-07-01 19:53:20 -0700

    feat: generate 'GetPostsForUser' sql->go code

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -3,3 +3,11 @@ INSERT INTO posts (id, created_at, updated_at, title, url, description, publishe
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 RETURNING *;
 --
+
+-- name: GetPostsForUser :many
+SELECT posts.* FROM posts
+JOIN feed_follows ON feed_follows.feed_id = posts.feed_id
+WHERE feed_follows.user_id = $1
+ORDER BY posts.published_at DESC
+LIMIT $2;
+--
